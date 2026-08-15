f213581fd46f4628bb577a609ff9b877191fb135
Edilberto Antonio Gómez García <edilbertoantoniogg@gmail.com>
2026-08-14 20:51:56 -0600

    distancia entre conjuntos realizada

--- mineria de datos/distance_sets.py
+++ mineria de datos/distance_sets.py
@@ -1,6 +1,6 @@
 import numpy as np
+import pandas as pd
 # from scipy.spatial import distance
-# distance.euclidean(a,b)
 
 # sets
 A = np.array([[1,1],[2,2],[1,2]])
@@ -14,55 +14,61 @@ def euc_dist(point1, point2):
 
 def distance_matrix(A,B):
     matrix = []
-
     for point_A in A:
         row = [];
         for point_B in B:
-            dist = euc_dist(point_A,point_B)
+            dist = euc_dist(point_A,point_B) # distance.euclidean(A,B)
             row.append(dist)
         matrix.append(row)
-
     return matrix
 
-def min_distance(A,B):
-    distance = []
-
-    for point_A in A:
-        aux = [];
-        for point_B in B:
-            dist = euc_dist(point_A,point_B)
-            aux.append(dist)
-        distance.append(min(aux))
-
+def list_min_dist(A,B):
+    matrix = distance_matrix(A,B)
+    distance = [float(min(row)) for row in matrix]
     return distance
 
-def max_distance(A,B):
-    distance = []
-
-    for point_A in A:
-        aux = [];
-        for point_B in B:
-            dist = euc_dist(point_A,point_B)
-            aux.append(dist)
-        distance.append(max(aux))
-
+def list_max_dist(A,B):
+    matrix = distance_matrix(A,B)
+    distance = [float(max(row)) for row in matrix]
     return distance
 
 def average_distance(A,B):
-    distance = []
+    matrix = distance_matrix(A,B)
+    avg = float(np.mean(matrix))
+    return avg
 
-    for point_A in A:
-        for point_B in B:
-            dist = euc_dist(point_A,point_B)
-            distance.append(dist)
+def dist_between_avgs(A, B):
+    avg_A = np.mean(A, axis=0)
+    avg_B = np.mean(B, axis=0)
+    dist = float(euc_dist(avg_A, avg_B))
 
-    return sum(distance)/len(distance)
-        
+    return dist
 
-prueba = average_distance(A,B)
-print(prueba)
+def hausdorff_distance(A, B):
+    max_AB = max(list_min_dist(A,B))
+    max_BA = max(list_min_dist(B,A))
+    dist = max(max_AB, max_BA)
+    return dist
+
+resultados = [min(list_min_dist(A,B)),
+            max(list_min_dist(A,B)),
+            average_distance(A,B),
+            dist_between_avgs(A,B),
+            hausdorff_distance(A,B)]
+
+resp = pd.DataFrame({'Distance':resultados})
+resp.index = ['Mininum distance over all pairs', 'Maximum distance over all pairs', 'Average distance over all pairs',
+            'Distance between averages', 'Hausdorff distance']
+
+print(resp)
 
 
+# prueba1 = list_min_dist(A,B)
+# print(prueba1)
+# prueba2 = list_min_dist(B,A)
+# print(prueba2)
+# prueba = hausdorff_distance(A,B)
+# print(prueba)
 
 # def min_all_pairs(A,B):
 
